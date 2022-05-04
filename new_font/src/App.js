0255e1e627952e72a0268a68c980f3a931668892
WZH119010324 <82816232+WZH119010324@users.noreply.github.com>
2022-05-05 01:04:02 +0800

    创建了新前端：new_font

--- new_font/src/App.js
+++ new_font/src/App.js
@@ -9,19 +9,17 @@ import { MenuUnfoldOutlined,
   BellOutlined,AppstoreOutlined,SettingOutlined, RocketOutlined} from '@ant-design/icons';
 import 'antd/dist/antd.css';
 import { useState, useEffect } from 'react';
-
-const { Option } = Select;
-const { TextArea } = Input;
-const { Header, Sider, Content } = Layout;
-const {Meta} = Card;
+import axios from 'axios';
 
 
 
 
 function App() {
-  
-  
-
+  const { Option } = Select;
+  const { TextArea } = Input;
+  const { Header, Sider, Content } = Layout;
+  const {Meta} = Card;
+  const [displayPage, setDisplayPage] = useState('home');
   return (
     <div className='page-1'>
        <Menu
@@ -33,18 +31,15 @@ function App() {
             defaultOpenKeys={['sub1']}
             mode="inline"
           >
-            <Menu.Item key="1"  icon={<HomeOutlined size={18}></HomeOutlined>}>HomePage</Menu.Item>
-            <Menu.Item key="2"  icon={<UserOutlined size={18} ></UserOutlined>}>Friend</Menu.Item>
-            <Menu.Item key="3"  icon={<BellOutlined size={18} ></BellOutlined>}>Message</Menu.Item>
-            <Menu.Item key="4"  icon={<RocketOutlined size={18} />}>Planet</Menu.Item>
-            <Button className='newMoment' type="primary" shape="round" >New Moment</Button>
+            <Menu.Item key="1" onClick ={() =>{setDisplayPage('home');}} icon={<HomeOutlined size={18}></HomeOutlined>}>HomePage</Menu.Item>
+            <Menu.Item key="2" onClick ={() =>{setDisplayPage('friend');}} icon={<UserOutlined size={18} ></UserOutlined>}>Friend</Menu.Item>
+            <Menu.Item key="3" onClick ={() =>{setDisplayPage('message')}} icon={<BellOutlined size={18} ></BellOutlined>}>Message</Menu.Item>
+            <Menu.Item key="4" onClick ={() =>{setDisplayPage('planet')}} icon={<RocketOutlined size={18} />}>Planet</Menu.Item>
         </Menu>
-        <div className='showPage'>
-        <Button className='newMoment' type="primary" shape="round" >Click To show The data</Button>
-        </div>
+        
+      
+
     </div>
-
-
   );
 }
 
