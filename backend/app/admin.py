11253d3d3a94ad2c0004562ec3f21d3ebe893043
WZH119010324 <82816232+WZH119010324@users.noreply.github.com>
2022-05-06 19:42:57 +0800

    在admin里注册创建的entity

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from app.models import Customer, Product, Address, Order, customer_postal
+
+admin.site.register([Customer, Product, Order, Address, customer_postal])
+# admin.site.register(Product)
+# admin.site.register(Order)
+# admin.site.register(Address)
+# admin.site.register(customer_postal)
 
 # Register your models here.
